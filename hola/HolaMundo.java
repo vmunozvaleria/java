7231df4e0ee102bc4157b159027a4354c7ab1ced
Valentina Muñoz <vmunozvaleria@icloud.com>
2022-05-05 15:33:11 -0400

    mi primer HolaMundo en java

---
+++ hola/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo { 
+
+    /** 
+     * @param args argumentos de la linea de comandos
+     */ 
+    public static void main(String[] args) { 
+
+        //Se imprime el mensaje
+        System.out.print("Hola Mundo - Valecita"); 
+    } 
+}
